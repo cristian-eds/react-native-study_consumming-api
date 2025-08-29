168f3c956166711793cb4f128e4bc33173c41cff
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-08-28 22:15:51 -0300

    Created strtucture for CardView component

---
+++ src/components/CardView/CardView.tsx
@@ -0,0 +1,13 @@
+import { View, Text, Button, Image } from 'react-native'
+
+import { styles } from './CardViewStyles'
+
+const CardView = () => {
+    return (
+        <View style={styles.containerImage}>
+            <Text>CardView</Text>
+        </View>
+    )
+}
+
+export default CardView
---
+++ src/components/CardView/CardViewStyles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    containerImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden',
+        borderRadius: 8
+    }
+})
+
